1482e306b770f7833ad9c824df0005b380c8c9f5
mosin74 <skmosin70201566@gmail.com>
2023-09-03 23:31:19 +0530

    LifitngTHingUp

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,16 +1,49 @@
+import { useState } from "react";
 
-import ClassComp from "./ClassComp";
+const Section = ({ title, info }) => {
 
+    const [isvisible, SetIsvisible] = useState(false);
+    return (
+        <>
+            <hr />
+            <h3>{title}</h3>
+            {
+                !isvisible ?<button onClick={() => { SetIsvisible(true) }}>Show</button> 
+                : 
+                <button onClick={() => { SetIsvisible(false) }}>Hide</button>
+            }
+            {isvisible && <p>{info}</p>}
+            <hr />
 
+        </>
+    )
+
+};
 const Contact = () => {
+
 
     return (
         <>
-            <h1>Contact us </h1>
-                <button>CLick me</button>
-            <hr />
-            <ClassComp name={"Mosin" }/>
 
+            <Section
+                title={"YOU can do it"}
+                info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            />
+
+            <Section
+                title={"YOU can do it"}
+                info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            />
+
+            <Section
+                title={"I can "}
+                info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            />
+
+            <Section
+                title={"AND I WILL "}
+                info={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+            />
 
 
 
