232c387909f733aa3869cd3472994974edfde6c0
mosin74 <skmosin70201566@gmail.com>
2023-09-19 21:15:44 +0530

    CSS

--- src/component/RestaurantCard.js
+++ src/component/RestaurantCard.js
@@ -6,8 +6,8 @@ import { IMG_CDN_URL } from "../config";
         <div className="Card ">
             <img className="IMAGE"src={IMG_CDN_URL+cloudinaryImageId} />
             <h3>{name}</h3>
+            <p1 className="avgRating"><span><img className="Rating" src={"https://logodix.com/logo/710633.png"} alt="star" /></span>{avgRating}</p1>        
             <p className="cuisines">{cuisines.join(", ")}</p>
-            <p1>{avgRating}star</p1>        
         </div>
     );
 };
