7be47b57090c31bc7318eafaddc07877c448c42e
mosin74 <skmosin70201566@gmail.com>
2023-09-22 19:39:29 +0530

    darkMode

---
+++ src/component/DarkThem.js
@@ -0,0 +1,40 @@
+import logo1 from "../Images/dark.png"
+import logo2 from "../Images/light.png"
+import { useEffect, useState } from "react"
+const DarkThem = () => {
+
+
+
+    // const ToggleDark = () => {
+    //     (Dark === "light-them")?
+    //         setDark("dark-them")
+    //         :
+    //         setDark("light-them")
+
+    // }
+
+
+    const [Dark, setDark] = useState("light-them");
+    const[isDarkTrue,setDarkTrue]=useState(false);
+    // const [isDark, setIsdark] = useState("dark-them")
+    useEffect(() => {
+        document.body.className = Dark;
+    },[Dark]);
+
+    
+    return (<>
+        {
+            isDarkTrue?
+                <>
+                    <img className="Them-logo"onClick={() =>{setDark("light-them"); setDarkTrue(false)}} src={logo2} alt="light" />
+                </>
+                :
+                <>
+                    <img className="Them-logo"onClick={() =>{setDark("dark-them") ; setDarkTrue(true)}} src={logo1}alt="Dark" />
+                </>
+        }
+    </>)
+
+}
+
+export default DarkThem;
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,31 +2,32 @@
 import { useState } from "react";
 import { ZomatoLogo, restaurantList } from "../config";
 import { Link } from "react-router-dom";
+import DarkThem from "./DarkThem";
 
 // const [Dark,SetDark]=useState()
 
 export const HeaderComponent = () => {
 
 
-const [Dark,SetDark]=useState(false)
 
 
     return (
         <>
 
             <div className="nav-bar">
-                <Link to="/"><img  src={ZomatoLogo} alt="" className="logo" /></Link>
+                <Link to="/"><img src={ZomatoLogo} alt="" className="logo" /></Link>
 
                 <ul>
-                    <li> <Link to="/Search">🔍Search</Link></li>
+                    <li> <Link to="/Search" className="Link">🔍Search</Link></li>
                     {/* <li> <Link to="/">Home</Link></li> */}
-                    <li> <Link to="/Help">Help</Link></li>
+                    <li> <Link to="/Help" className="Link">Help</Link></li>
                     {/* <li> <Link to="/About">About us</Link></li> */}
                     {/* <li><Link to="/Contact">Contact</Link></li> */}
                     <li>Cart</li>
                 </ul>
-                {Dark ? <img  onClick ={ ()=>{SetDark(false)}} src="" alt="Dark" className="logo" />: <img  onClick ={ ()=>{SetDark(true)}}src="" alt="Light" className="logo" />}
+                <DarkThem/>
             </div>
+
 
         </>
     );
